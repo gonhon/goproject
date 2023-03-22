d70565b20f254bf036527a5fdd58c70976ff7996
高建成 <gaojiancheng@shwpg.com>
2023-03-22 19:17:34 +0800

    test

---
+++ geeweb/gee/trie_test.go
@@ -0,0 +1,11 @@
+package gee
+
+func newTestRouter() *router {
+	r := newRouter()
+	r.AddRouter("GET", "/", nil)
+	r.AddRouter("GET", "/hello/:name", nil)
+	r.AddRouter("GET", "/hello/b/c", nil)
+	r.AddRouter("GET", "/hi/:name", nil)
+	r.AddRouter("GET", "/assets/*filepath", nil)
+	return r
+}
