95c6f75178d0a97d13638bcd5ad0e08ab83719c8
gaoh <5146513+gaohwh@user.noreply.gitee.com>
2023-03-30 00:05:26 +0800

    group

--- gee/cache/geecache.go
+++ gee/cache/geecache.go
@@ -1,5 +1,10 @@
 package cache
 
+import (
+	"fmt"
+	"sync"
+)
+
 //接口形函数
 type Getter interface {
 	Get(key string) ([]byte, error)
@@ -9,4 +14,71 @@ type GetterFunc func(string) ([]byte, error)
 
 func (f GetterFunc) Get(key string) ([]byte, error) {
 	return f(key)
+}
+
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache cache
+}
+
+var (
+	//使用读写锁
+	mutex  sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("getter is nil")
+	}
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	group := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = group
+	return group
+}
+
+func GetGroup(name string) *Group {
+	mutex.RLock()
+	defer mutex.RUnlock()
+	return groups[name]
+}
+
+//加入元素
+func (group *Group) populateCache(key string, val ByteView) {
+	group.mainCache.Add(key, val)
+}
+
+//回调Get方法初始化缓存
+func (group *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := group.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	val := ByteView{b: cloneBytes(bytes)}
+	group.populateCache(key, val)
+	return val, nil
+}
+
+func (group *Group) load(key string) (val ByteView, err error) {
+	return group.getLocally(key)
+}
+
+//根据key获取数据
+func (group *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is empty")
+	}
+	//存在及从缓存取
+	if v, ok := group.mainCache.Get(key); ok {
+		return v, nil
+	}
+	//不存在调用Getter
+	return group.load(key)
 }
--- gee/cache/geecache_test.go
+++ gee/cache/geecache_test.go
@@ -1,6 +1,7 @@
 package cache
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -16,4 +17,42 @@ func TestFunc(t *testing.T) {
 	} else {
 		t.Logf("val:%s", string(v))
 	}
+}
+
+func TestGroupRun(t *testing.T) {
+
+	var base = map[string]string{
+		"key1": "value1",
+		"key2": "value2",
+		"key3": "value4",
+	}
+	//存储个数
+	loadCounts := make(map[string]int, len(base))
+	group := NewGroup("key", 2<<5, GetterFunc(func(key string) ([]byte, error) {
+		t.Logf("init %s data ...", key)
+		//从base中取出 模拟数据库
+		if val, ok := base[key]; ok {
+			if _, ok := loadCounts[key]; !ok {
+				loadCounts[key] = 0
+			}
+			loadCounts[key] += 1
+			return []byte(val), nil
+		}
+		return nil, fmt.Errorf("%s not found", key)
+	}))
+
+	for k, v := range base {
+		if view, err := group.Get(k); err != nil || view.String() != v {
+			t.Logf("%s val not match", k)
+		}
+
+		if _, err := group.Get(k); err != nil || loadCounts[k] > 1 {
+			t.Logf("   %s not found", k)
+		}
+	}
+
+	if view, err := group.Get("key6"); err == nil {
+		t.Logf("the value of unknow should be empty, but %s got", view)
+	}
+
 }
