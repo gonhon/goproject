a851d3204ddb8963ab0e46e863097c97acdbc6e5
高建成 <gaojiancheng@shwpg.com>
2023-03-30 08:31:18 +0800

    group test

--- gee/cache/geecache_test.go
+++ gee/cache/geecache_test.go
@@ -24,20 +24,21 @@ func TestGroupRun(t *testing.T) {
 	var base = map[string]string{
 		"key1": "value1",
 		"key2": "value2",
-		"key3": "value4",
+		"key3": "value3",
 	}
 	//存储个数
 	loadCounts := make(map[string]int, len(base))
 	group := NewGroup("key", 2<<5, GetterFunc(func(key string) ([]byte, error) {
-		t.Logf("init %s data ...", key)
 		//从base中取出 模拟数据库
 		if val, ok := base[key]; ok {
+			t.Logf("init %s data ...", key)
 			if _, ok := loadCounts[key]; !ok {
 				loadCounts[key] = 0
 			}
 			loadCounts[key] += 1
 			return []byte(val), nil
 		}
+		t.Logf("%s not  data ...", key)
 		return nil, fmt.Errorf("%s not found", key)
 	}))
 
@@ -51,8 +52,9 @@ func TestGroupRun(t *testing.T) {
 		}
 	}
 
-	if view, err := group.Get("key6"); err == nil {
-		t.Logf("the value of unknow should be empty, but %s got", view)
+	if v, err := group.Get("key1"); err == nil {
+		t.Log(v.String())
 	}
+	group.Get("key6")
 
 }
