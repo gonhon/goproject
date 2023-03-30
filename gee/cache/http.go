aa663ca484b8d7b65cd7a4bf7be5c84853025095
高建成 <gaojiancheng@shwpg.com>
2023-03-30 12:45:07 +0800

    http server

---
+++ gee/cache/http.go
@@ -0,0 +1,55 @@
+package cache
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const defaultBasePath = "/cache"
+
+type HttpPoll struct {
+	self     string
+	basePath string
+}
+
+func NewHttpPoll(self string) *HttpPoll {
+	return &HttpPoll{self: self, basePath: defaultBasePath}
+}
+
+func (p *HttpPoll) Log(format string, args ...interface{}) {
+	log.Printf("server %s: %s", p.self, fmt.Sprintf(format, args...))
+}
+
+func (p *HttpPoll) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, p.basePath) {
+		panic("HttpPoll serving unexpected path: " + r.URL.Path)
+	}
+
+	p.Log("%s %s", r.Method, r.URL.Path)
+
+	paths := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
+
+	if len(paths) != 2 {
+		http.Error(w, "request failed", http.StatusBadRequest)
+		return
+	}
+	// group :=
+	// key :=
+
+	group := GetGroup(paths[0])
+	if group == nil {
+		http.Error(w, "group not found", http.StatusNotFound)
+		return
+	}
+
+	view, err := group.Get(paths[1])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	//写出文件
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(view.ByteSlice())
+}
---
+++ gee/cache/http_test.go
@@ -0,0 +1,36 @@
+package cache
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestCacheHttp(t *testing.T) {
+	var base = map[string]string{
+		"key1": "value1",
+		"key2": "value2",
+		"key3": "value3",
+	}
+	//存储个数
+	loadCounts := make(map[string]int, len(base))
+	NewGroup("key", 2<<5, GetterFunc(func(key string) ([]byte, error) {
+		//从base中取出 模拟数据库
+		if val, ok := base[key]; ok {
+			t.Logf("init %s data ...", key)
+			if _, ok := loadCounts[key]; !ok {
+				loadCounts[key] = 0
+			}
+			loadCounts[key] += 1
+			return []byte(val), nil
+		}
+		t.Logf("%s not  data ...", key)
+		return nil, fmt.Errorf("%s not found", key)
+	}))
+
+	addr := "127.0.0.1:8900"
+	httpPoll := NewHttpPoll(addr)
+	t.Log("addr", addr)
+	http.ListenAndServe(addr, httpPoll)
+
+}
