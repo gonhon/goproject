44bcb31b5c038b6f0f6e943296dd216d68e24109
gaoh <gaojiancheng@shwpg.com>
2023-04-14 13:06:27 +0800

    log

---
+++ gee/orm/log/log.go
@@ -0,0 +1,51 @@
+package log
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"sync"
+)
+
+// [info ] 颜色为蓝色，[error] 为红色。
+// 使用 log.Lshortfile 支持显示文件名和代码行号。
+var (
+	errorLog = log.New(os.Stdout, "\033[31m[error]\033[0m ", log.LstdFlags|log.Lshortfile)
+	infoLog  = log.New(os.Stdout, "\033[31m[info]\033[0m ", log.LstdFlags|log.Lshortfile)
+	loggers  = []*log.Logger{errorLog, infoLog}
+	mutex    = sync.Mutex{}
+)
+
+// methods
+var (
+	Error  = errorLog.Println
+	Errorf = errorLog.Printf
+	Info   = infoLog.Println
+	Infof  = infoLog.Printf
+)
+
+// levels
+
+const (
+	InfoLevel = iota
+	ErrorLevel
+	DisableLevel
+)
+
+// 如果设置为 ErrorLevel，infoLog 的输出会被定向到 ioutil.Discard，即不打印该日志。
+func SetLevel(level int) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	for _, logger := range loggers {
+		logger.SetOutput(os.Stdout)
+	}
+
+	if ErrorLevel < level {
+		errorLog.SetOutput(ioutil.Discard)
+	}
+
+	if InfoLevel < level {
+		infoLog.SetOutput(ioutil.Discard)
+	}
+}
