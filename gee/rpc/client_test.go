739bc5e03020ac753113a8c1de1729e2f02d83ca
gaoh <gaojiancheng@shwpg.com>
2023-05-31 08:46:03 +0800

    test

--- gee/rpc/client_test.go
+++ gee/rpc/client_test.go
@@ -17,15 +17,20 @@ func TestClient_dialTimeout(t *testing.T) {
 	t.Parallel()
 	l, _ := net.Listen("tcp", ":0")
 
+	//耗时2s
 	f := func(conn net.Conn, opt *Option) (client *Client, err error) {
 		_ = conn.Close()
 		time.Sleep(time.Second * 2)
 		return nil, nil
 	}
+
+	//耗时1s
 	t.Run("timeout", func(t *testing.T) {
 		_, err := dialTimeout(f, "tcp", l.Addr().String(), &Option{ConnectTimeout: time.Second})
 		_assert(err != nil && strings.Contains(err.Error(), "connect timeout"), "expect a timeout error")
 	})
+
+	//耗时0s
 	t.Run("0", func(t *testing.T) {
 		_, err := dialTimeout(f, "tcp", l.Addr().String(), &Option{ConnectTimeout: 0})
 		_assert(err == nil, "0 means no limit")
@@ -54,6 +59,7 @@ func TestClient_Call(t *testing.T) {
 	go startServer(addrCh)
 	addr := <-addrCh
 	time.Sleep(time.Second)
+	//客户端设置超时时间为 1s，服务端无限制
 	t.Run("client timeout", func(t *testing.T) {
 		client, _ := Dial("tcp", addr)
 		ctx, _ := context.WithTimeout(context.Background(), time.Second)
@@ -61,8 +67,11 @@ func TestClient_Call(t *testing.T) {
 		err := client.Call(ctx, "Bar.Timeout", 1, &reply)
 		_assert(err != nil && strings.Contains(err.Error(), ctx.Err().Error()), "expect a timeout error")
 	})
+
+	//服务端设置超时时间为1s，客户端无限制
 	t.Run("server handle timeout", func(t *testing.T) {
 		client, _ := Dial("tcp", addr, &Option{
+			//服务端超时时间
 			HandleTimeout: time.Second,
 		})
 		var reply int
