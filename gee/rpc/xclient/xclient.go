a60422dc7b127c31c7e2646331dafd14110564a3
gaoh <5146513+gaohwh@user.noreply.gitee.com>
2023-06-07 23:26:03 +0800

    xclient

--- gee/rpc/xclient/xclient.go
+++ gee/rpc/xclient/xclient.go
@@ -1,7 +1,9 @@
 package xclient
 
 import (
+	"context"
 	"io"
+	"reflect"
 	"sync"
 
 	"github.com/limerence-code/goproject/gee/rpc"
@@ -30,4 +32,74 @@ func (xc *XClient) Close() error {
 		delete(xc.clients, key)
 	}
 	return nil
+}
+
+func (xc *XClient) dial(rpcAddr string) (*rpc.Client, error) {
+	xc.mutex.Lock()
+	defer xc.mutex.Unlock()
+	client, exist := xc.clients[rpcAddr]
+	if exist && !client.IsAvailable() {
+		client.Close()
+		delete(xc.clients, rpcAddr)
+		client = nil
+	}
+	if client == nil {
+		// var err error
+		client, err := rpc.XDial(rpcAddr, xc.opt)
+		if err != nil {
+			return nil, err
+		}
+		xc.clients[rpcAddr] = client
+	}
+	return client, nil
+}
+
+func (xc *XClient) call(rpcAddr string, ctx context.Context, serviceMethod string, args, reply interface{}) error {
+	client, err := xc.dial(rpcAddr)
+	if err != nil {
+		return nil
+	}
+	return client.Call(ctx, serviceMethod, args, reply)
+}
+func (xc *XClient) Call(ctx context.Context, serviceMethod string, args, reply interface{}) error {
+	rpcAddr, err := xc.d.Get(xc.mode)
+	if err != nil {
+		return err
+	}
+	return xc.call(rpcAddr, ctx, serviceMethod, args, reply)
+}
+
+func (xc *XClient) Broadcast(ctx context.Context, serviceMethod string, args, reply interface{}) error {
+	services, err := xc.d.GetAll()
+	if err != nil {
+		return err
+	}
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
+	var e error
+	replyDone := reply == nil
+	ctx, cancel := context.WithCancel(ctx)
+	for _, rpcAddr := range services {
+		wg.Add(1)
+		go func(rpcAddr string) {
+			defer wg.Done()
+			var cloneReply interface{}
+			if reply != nil {
+				cloneReply = reflect.New(reflect.ValueOf(reply).Elem().Type()).Interface()
+			}
+			err := xc.call(rpcAddr, ctx, serviceMethod, args, cloneReply)
+			mutex.Lock()
+			if err != nil && e == nil {
+				e = err
+				cancel()
+			}
+			if err == nil && !replyDone {
+				reflect.ValueOf(reply).Elem().Set(reflect.ValueOf(cloneReply).Elem())
+				replyDone = true
+			}
+			mutex.Unlock()
+		}(rpcAddr)
+	}
+	wg.Wait()
+	return e
 }
