30281d26b621b0d5ff7ffc3e2ad0c64909e5bc42
gaoh <2199382542@qq.com>
2024-07-19 12:43:59 +0800

    http & test

---
+++ middleware/gin/gin-use/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main() {
+	var sl = [][]int{
+		{1, 34, 26, 35, 78},
+		{3, 45, 13, 24, 99},
+		{101, 13, 38, 7, 127},
+		{54, 27, 40, 83, 81},
+	}
+outerloop:
+	for i := 0; i < len(sl); i++ {
+		for j := 0; j < len(sl[i]); j++ {
+			if sl[i][j] == 13 {
+				fmt.Printf("found 13 at [%d, %d]\n", i, j)
+				break outerloop
+			}
+		}
+	}
+
+	var m = map[string]int{
+		"tony": 21,
+		"tom":  22,
+		"jim":  23,
+	}
+	counter := 0
+	for k, v := range m {
+		if counter == 0 {
+			delete(m, "tony")
+		}
+		counter++
+		fmt.Println(k, v)
+	}
+	fmt.Println("counter is ", counter)
+
+	counter = 0
+	for k, v := range m {
+		if counter == 0 {
+			m["lucy"] = 24
+		}
+		counter++
+		fmt.Println(k, v)
+	}
+	fmt.Println("counter is ", counter)
+}
